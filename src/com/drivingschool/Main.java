8d92b7749d0ba550b4605f7771e3eb5e54cbcf65
Dovud <dinomov99@gmail.com>
2021-01-03 23:19:40 +0200

    tesing Student class

--- src/com/drivingschool/Main.java
+++ src/com/drivingschool/Main.java
@@ -1,10 +1,27 @@
 package com.drivingschool;
 
+import com.drivingschool.entity.Student;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.printf("\n Tests for Class Attendance\n\n");
+    public static void main(String[] args) throws ParseException {
+        String dateString = "01/08/1985";
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Date dob = formatter.parse(dateString);
+        Student student = new Student("177", "Dovud", "Inomov", "Tajik", dob, "New student",  new Date());
+
+        System.out.printf("%10s %15s %15s %10s %10s %10s %10s",
+                student.getID(),
+                student.getName(),
+                student.getSurname(),
+                student.getNationality(),
+                student.getDOB(),
+                student.getStatus(),
+                student.getRegisterDate());
 
     }
 }
